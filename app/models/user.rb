7971d5b2fe664f622d8d5b5cc898f812d3d3e358
tallkeith <keithpnash@gmail.com>
2021-02-15 21:53:58 -0500

    Add bidirectional relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_many :matches
+  has_many :matched_users, through: :matches, dependent: :destroy do
+    def with_match_data
+      select('users.*, matches.created_at AS match_created_at')
+    end
+  end
 end
---
+++ spec/factories/matches.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :match do
+    user nil
+    matched_user nil
+  end
+
+end
